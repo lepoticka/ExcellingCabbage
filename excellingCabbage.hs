5d46d53666ac1feadee8c934c75b657300ce8d6b
Luka Krapic <lk6816@student.fmf.uni-lj.si>
2014-12-03 21:29:53 +0100

    Add commented reactive banana example to the repository.
    Reason:
    - unable to install reactive-banana-wx module due to
            array-0.4.0.1 requirement

--- excellingCabbage.hs
+++ excellingCabbage.hs
@@ -1,5 +1,9 @@
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
+-- import Data.Maybe
+-- import Graphics.UI.WX hiding (Event)
+-- import Reactive.Banana
+-- import Reactive.Banana.WX
 
 expr :: Parser Integer
 expr = buildExpressionParser table factor <?> "expression"
@@ -16,3 +20,34 @@ number :: Parser Integer
 number = do { ds <- many1 digit; return (read ds) } <?> "number"
 
 -- Test: parseTest expr "2+3*5"
+--
+-- main :: IO ()
+-- main = start $ do
+--     f           <- frame [ text := "Arithmetic"]
+--     input1      <- entry f []
+--     input2      <- entry f []
+--     output      <- staticText f []
+--
+--     set f [layout := margin IO $ row IO
+--             [widget input1, label "+" , widget input2
+--             , label "=", minsize (sz 40 20) $ widget output ]]
+--
+--     let networkDescription :: forall t. Frameworks t => Moment t ()
+--         networkDescription = do
+--
+--         binput1 <- behaviorText input1 ""
+--         binput2 <- behaviorText input2 ""
+--
+--         let 
+--             result :: Behavior t (Maybe Int)
+--             result = f <$> binput1 <*> binput2
+--                 where
+--                 f x y = liftA2 (+) (readNumber x) (readNumber y)
+--
+--             readNumber s = listToMaybe [x | (x, "") <- reads s]
+--             showNumber   = maybe "--" show
+--
+--         sink output [text :== showNumber <$> result]
+--
+--     network <- compile networkDescription
+--     actuate network
