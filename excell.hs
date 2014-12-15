4432926954d325e46c7972e034d9f349b5029e03
Luka Krapic <lk6816@student.fmf.uni-lj.si>
2014-12-15 19:26:45 +0100

    Add multiple cells without reactive component.

--- excell.hs
+++ excell.hs
@@ -1,4 +1,4 @@
-import Control.Monad    (void)
+import Control.Monad    (void, replicateM)
 import Data.Maybe
 import Text.Printf
 import Safe             (readMay)
@@ -6,6 +6,7 @@ import  qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 
 import qualified ExcellingCabbage as EC
+import Data.Char
 
 parserBehavior :: Behavior (String -> EC.Expression)
 parserBehavior = pure (EC.processParse . EC.parseArithmetic)
@@ -16,33 +17,44 @@ evaluateBehavior = pure EC.evaluate
 convertBehavior :: Behavior (Integer -> String)
 convertBehavior = pure show
 
+makeL :: [[Element]] -> UI Element
+makeL field = grid $ map string letters : [string ( show num) : map element (field !! (num -1))  | num <- [1..height]]
+  where
+    letters = "" : [chr  ( ord 'a' + num - 1 ) :  show num | num <- [1..width]]
+    height = length field
+    width = length $ head field
 
 
 main::IO()
-main =  startGUI defaultConfig setup
+main =  startGUI defaultConfig $ setup 5 5
 
-setup :: Window -> UI ()
-setup window = void $ do
-    return window # set title "Excell"
+setup :: Int -> Int -> Window -> UI ()
+setup width heigth window = void $ do
+  return window # set title "Excell"
 
-    inputCelica     <- UI.input
-    outputCelica    <- UI.input
+  elementField  <- replicateM heigth $ replicateM width UI.input
 
-    getBody window #+ [
-            column [
-                grid [[string "Input celica", element inputCelica]
-                    , [string "Output celica", element outputCelica]]
-            ]]
+  getBody window #+ [ column [makeL elementField]]
 
-    let 
-        procEvent           =  apply parserBehavior $ UI.valueChange inputCelica
-        evalEvent           =  apply evaluateBehavior procEvent
-        convertEvent        = apply convertBehavior evalEvent
 
-    procBehavior            <- stepper (EC.Constant 0) procEvent
-    evalBehavior            <- stepper 0 evalEvent
-    convertBehavior         <- stepper "0" convertEvent
-
-    -- inputIn <- stepper "0" $ UI.valueChange inputCelica
-
-    element outputCelica # sink value convertBehavior
+    -- inputCelica     <- UI.input
+    -- outputCelica    <- UI.input
+    --
+    -- getBody window #+ [
+    --         column [
+    --             grid [[string "Input celica", element inputCelica]
+    --                 , [string "Output celica", element outputCelica]]
+    --         ]]
+    --
+    -- let 
+    --     procEvent           =  apply parserBehavior $ UI.valueChange inputCelica
+    --     evalEvent           =  apply evaluateBehavior procEvent
+    --     convertEvent        = apply convertBehavior evalEvent
+    --
+    -- procBehavior            <- stepper (EC.Constant 0) procEvent
+    -- evalBehavior            <- stepper 0 evalEvent
+    -- convertBehavior         <- stepper "0" convertEvent
+    --
+    -- -- inputIn <- stepper "0" $ UI.valueChange inputCelica
+    --
+    -- element outputCelica # sink value convertBehavior
