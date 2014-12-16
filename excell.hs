688f5f547d57aefd6c349a2a8025c6a69c455638
Luka Krapic <lk6816@student.fmf.uni-lj.si>
2014-12-16 01:40:56 +0100

    mend

--- excell.hs
+++ excell.hs
@@ -1,13 +1,8 @@
 import Control.Monad    (void)
-import Data.Maybe
-import Text.Printf
-import Safe             (readMay)
 import  qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
-import Data.Tuple
 
 import qualified ExcellingCabbage as EC
-import Reactive.Threepenny
 
 parserBehavior :: Behavior (String -> EC.Expression)
 parserBehavior = pure (EC.processParse . EC.parseArithmetic)
@@ -37,22 +32,16 @@ setup window = void $ do
             ]]
 
     buffer <- stepper (EC.Constant 0) $ apply parserBehavior $ UI.valueChange inputCelica
-
-   -- naredi flush event String
-    -- event ki reagira na enter
-    -- on enter event daj na flushevent buffer
     flushpair <- liftIO newEvent :: UI(Event EC.Expression, Handler EC.Expression)
+
     let 
         flush = fst flushpair
         flushHandler = snd flushpair
 
-    -- na event podigni event v flush event streamu
     onEvent ( filterE (==13) (UI.keydown inputCelica)) $ \_ -> liftIO $ flushHandler =<< currentValue buffer
 
     let
-        evalEvent           =  apply evaluateBehavior flush
-        convertEvent        = apply convertBehavior evalEvent
+        convertEvent        = apply convertBehavior $ apply evaluateBehavior flush
 
     convertB         <- stepper "0" convertEvent
-
     element outputCelica # sink value convertB
